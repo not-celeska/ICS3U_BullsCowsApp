088d901369779065db02ee0c87ca6e42823ba053
not-celeska <bushkov.mikhail@gmail.com>
2023-12-30 20:41:27 -0500

    Initial commit

---
+++ app/src/main/java/com/example/ics3u_bullscowsapp/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.ics3u_bullscowsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void goToInfoMenu(View view) {
+//        Intent intent = new Intent(this, )
+    }
+
+}
